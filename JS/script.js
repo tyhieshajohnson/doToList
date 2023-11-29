a654af61d0846c516e1693be729321b14c0522fb
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-29 11:10:34 +0200

    cssComplete

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+// Navigation Bar:
+function toggleNavbar() {
+    var x = document.getElementById("navbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
